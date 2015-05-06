29d3dfb0b68ed503f83854a1d720c389cd5e1182
Murphy Randle <murphyrandle@gmail.com>
2015-05-06 11:59:33 -0600

    Murphy's incomplete refactor

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -1,55 +1,55 @@
+type Item {
+  name: string;
+  sell_in: number;
+  quality: number;
+}
+
 function Item(name, sell_in, quality) {
   this.name = name;
   this.sell_in = sell_in;
   this.quality = quality;
 }
 
-var items = []
+function extractByName(name:string, items:Array<Item>):Array<Item> {
+  // ...
+}
 
-function update_quality() {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
-    }
-  }
+function withoutByName(name:string, items:Array<Item>):Array<Item> {
+  // ...
+}
+
+function updateBackstagePasses(items:Array<Item>):Array<Item> {
+  // ...
+}
+
+function updateLegendaryItems(items:Array<Item>):Array<Item> {
+  // ...
+}
+
+function updateAgedBrieItems(items:Array<Item>):Array<Item> {
+  // ...
+}
+
+function updateConjuredItems(items:Array<Item>):Array<Item> {
+  // ...
+}
+
+function updateGeneralItems(items:Array<Item>):Array<Item> {
+  // ...
+}
+
+function updateQuality(items:Array<Item>):Array<Item> {
+  var backstagePasses = extractByName('Backstage passes to a TAFKAL80ETC concert', items)
+  backstagePasses = updateBackstagePasses(backstagePasses)
+  items = withoutByName('Backstage passes to a TAFKAL80ETC concert', items)
+
+  var brie = extractByName('Aged Brie', items)
+  brie = updateAgedBrieItems(brie)
+  items = withoutByName('Aged Brie')
+
+  // ... do the other special ones, blah blah
+
+  items = updateGeneralItems(items)
+
+  return concat([], backstagePasses, brie, items)
 }
